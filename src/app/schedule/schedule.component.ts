a6947bc783f48f0019ec7a83849051f0a441f7f7
Henry Oforeh <henry.oforeh@standardbank.co.za>
2017-09-05 21:00:31 +0200

    Updates

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule,FormControl } from '@angular/forms';
 import {DialogModule,DropdownModule,DataTableModule,SharedModule,ListboxModule,ScheduleModule} from 'primeng/primeng';
 import {PlaceServiceApi,ScheduleServiceApi,UserServiceApi} from '../shared/shared';
 import 'fullcalendar';
+import * as moment from 'moment';
 
 
 
@@ -19,11 +20,17 @@ export class ScheduleComponent implements OnInit {
   events : any[] =[];
 
   header: any;
+
+  defaultDate:string = moment().format('YYYY-MM-DD').toString();
+
+  
   
 
   constructor(private placeServiceApi:PlaceServiceApi ,
               private scheduleServiceApi:ScheduleServiceApi,
               private userServiceApi : UserServiceApi) {
+
+                console.log(this.defaultDate);
 
   }
 
@@ -33,31 +40,31 @@ export class ScheduleComponent implements OnInit {
 			center: 'title',
 			right: 'month,agendaWeek,agendaDay'
     };
-    
-    this.events = [
-      {
-          "title": "All Day Event",
-          "start": "2016-12-12"
-      },
-      {
-          "title": "Long Event",
-          "start": "2016-01-07",
-          "end": "2016-01-10"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2016-01-09T16:00:00"
-      },
-      {
-          "title": "Repeating Event",
-          "start": "2016-01-16T16:00:00"
-      },
-      {
-          "title": "Conference",
-          "start": "2016-01-11",
-          "end": "2016-01-13"
-      }
-  ];
+    this.listEventsApi();
+  //   this.events = [
+  //     {
+  //         "title": "All Day Event",
+  //         "start": "2016-12-12"
+  //     },
+  //     {
+  //         "title": "Long Event",
+  //         "start": "2016-01-07",
+  //         "end": "2016-01-10"
+  //     },
+  //     {
+  //         "title": "Repeating Event",
+  //         "start": "2016-01-09T16:00:00"
+  //     },
+  //     {
+  //         "title": "Repeating Event",
+  //         "start": "2016-01-16T16:00:00"
+  //     },
+  //     {
+  //         "title": "Conference",
+  //         "start": "2016-01-11",
+  //         "end": "2016-01-13"
+  //     }
+  // ];
   }
 
     listEventsApi(){
@@ -65,15 +72,25 @@ export class ScheduleComponent implements OnInit {
       this.scheduleServiceApi.getSchedules()
       .subscribe(
            res => {
-            //  for(var i=0; i< res.length; i++){
-            //    this.events.push({
-            //      title: res[i].
-            //    });
-             
+             for(var i=0; i< res.length; i++){
+                this.events.push({
+                    title: res[i].place.name,
+                    start: this.parseVisitDate(res[i].visitDate,res[i].visitTime)
+                 });
+              }
+             console.log(JSON.stringify(this.events));
            },err => {
              console.log(err.message);
              return;
          });
+    }
+
+    parseVisitDate(visitDateVar,visitTimeVar){
+       if(visitTimeVar===null){
+          return visitDateVar;
+       }else{
+         return visitTimeVar;
+       }
     }
 
 }
