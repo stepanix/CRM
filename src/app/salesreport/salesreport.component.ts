da221a9cb0b7fa59329eb33dade75c83310322ea
Henry Oforeh <henry.oforeh@standardbank.co.za>
2017-11-19 19:59:33 +0200

    updates

--- src/app/salesreport/salesreport.component.ts
+++ src/app/salesreport/salesreport.component.ts
@@ -19,16 +19,18 @@ export class SalesReportComponent implements OnInit {
   id: any;
   reportData: any;
   reportHeader: any;
-  
+
   dateFrom: any = "";
   dateTo: any = "";
 
   data: any;
-  barchartData : number[] = [];
-  barchartLabels : any[] = [];
-  tempLabelData : any[] = [];
+  barchartData: number[] = [];
+  barchartLabels: any[] = [];
+  tempLabelData: any[] = [];
 
   extractedTempData: any[] = [];
+  salesByProducts: any[] = [];
+  totalSalesPerDate: any[] = [];
 
   constructor(private orderServiceApi: OrderItemServiceApi,
     private router: Router,
@@ -55,59 +57,93 @@ export class SalesReportComponent implements OnInit {
     this.listSalesReportValues();
   }
 
-  // getRollingValue(label:string) : number{
-  //  let valueModel =  this.tempLabelData.find(item => item.label === label);
-  //  return parseFloat(valueModel.value);
-  // }
+  updateBarChartData(label, newValue: number) {
+    let itemModel: any = this.tempLabelData.find(item => item.label === label)
+    itemModel.value = parseFloat(itemModel.value) + newValue;
+  }
 
-  updateBarChartData(label,newValue:number) {
-   let itemModel : any =  this.tempLabelData.find(item => item.label === label)
-   itemModel.value = parseFloat(itemModel.value) + newValue;
-   //console.log("item index",index);
-    //let index: any = this.tempLabelData.indexOf(itemData);
-    //console.log("index",index);
-    // if (index !== -1) {
-    //   this.tempLabelData[index].value = newValue;
-    // }
+  updateTotalProductOrderValue(id: number, newValue: number) {
+    let itemModel: any = this.salesByProducts.find(item => item.id === id)
+    itemModel.totalSold = parseFloat(itemModel.totalSold) + newValue;
+  }
+
+  updateTotalSoldPerDate(productId: number, name: string, date: any, newValue: number) {
+    let itemModel: any = this.totalSalesPerDate.find(item => item.date === date && item.id === productId)
+    if (itemModel === undefined) {
+      this.totalSalesPerDate.push({
+        id: productId,
+        name: name,
+        date: date,
+        value: newValue
+      });
+    } else {
+      itemModel.value = parseFloat(itemModel.value) + newValue;
+    }
   }
 
   listSalesReportValues() {
     let tempReportData: any[] = [];
-    let sum: number = 0;    
-    let labelItem :string = "";
-    let valueItem : number = 0;
+    let sum: number = 0;
+    let labelItem: string = "";
+    let valueItem: number = 0;
     this.barchartData = [];
     this.barchartLabels = [];
+    this.salesByProducts = [];
+    this.totalSalesPerDate = [];
 
-    this.busy = this.orderServiceApi.getOrderItemsDateRange(this.dateFrom, this.dateTo)
+    this.busy = this.orderServiceApi
+      .getOrderItemsDateRange(this.dateFrom, this.dateTo)
       .subscribe(
       res => {
-        if(res.length > 0) {
+        if (res.length > 0) {
           for (var i = 0; i < res.length; i++) {
             labelItem = moment(res[i].addedDate).format("MMM Do");
-            if(this.tempLabelData.find(item => item.label === labelItem)===undefined) {
-               //add new item
-               this.tempLabelData.push({
-                  label : labelItem,
-                  value : res[i].amount
-               });
-            }else{
-               //update existing item
-               this.updateBarChartData(labelItem,parseFloat(res[i].amount));
+            if (this.tempLabelData.find(item => item.label === labelItem) === undefined) {
+              //add new item
+              this.tempLabelData.push({
+                label: labelItem,
+                value: res[i].amount
+              });
+            } else {
+              //update existing item
+              this.updateBarChartData(labelItem, parseFloat(res[i].amount));
+            }
+
+            if (this.salesByProducts.find(item => item.id === parseInt(res[i].product.id)) === undefined) {
+              this.salesByProducts.push({
+                id: res[i].product.id,
+                name: res[i].product.name,
+                totalSold: parseFloat(res[i].amount)
+              });
+            } else {
+              this.updateTotalProductOrderValue(parseInt(res[i].product.id), parseFloat(res[i].amount))
+            }
+            if (this.totalSalesPerDate.find(item => item.date === labelItem && item.id === parseInt(res[i].product.id)) === undefined) {
+              this.totalSalesPerDate.push({
+                id: parseInt(res[i].product.id),
+                name: res[i].product.name,
+                date: labelItem,
+                value: parseFloat(res[i].amount)
+              });
+            } else {
+              this.updateTotalSoldPerDate(parseInt(res[i].product.id), res[i].product.name, labelItem, parseFloat(res[i].amount));
             }
           }
+
+          console.log("sales by product", this.salesByProducts);
+          console.log("sales per date", this.totalSalesPerDate);
         }
-        
-        for(let x=0;x<this.tempLabelData.length;x++){
+
+        for (let x = 0; x < this.tempLabelData.length; x++) {
           this.barchartLabels.push(this.tempLabelData[x].label);
           this.barchartData.push(this.tempLabelData[x].value);
         }
-                
+
         this.data = {
           labels: this.barchartLabels,
           datasets: [
             {
-              label: 'Sales Report',
+              label: 'Sales Report by Product',
               backgroundColor: '#42A5F5',
               borderColor: '#1E88E5',
               data: this.barchartData
